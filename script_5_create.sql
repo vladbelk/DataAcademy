1bf8feb7b5fd75281ae790a13bfe2630ea7e6fd1
vladbelk <vladbelk321@gmail.com>
2024-03-23 13:17:26 +0200

    Meeting_5_Create

---
+++ script_5_create.sql
@@ -0,0 +1,107 @@
+CREATE DATABASE Academy_5;
+GO
+
+USE Academy_5;
+GO
+
+CREATE TABLE Assistants (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    TeacherId INT NOT NULL,
+    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE Curators (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    TeacherId INT NOT NULL,
+    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE Deans (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    TeacherId INT NOT NULL,
+    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE Departments (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Name NVARCHAR(100) NOT NULL UNIQUE,
+    FacultyId INT NOT NULL,
+    HeadId INT NOT NULL,
+    FOREIGN KEY (FacultyId) REFERENCES Faculties(Id),
+    FOREIGN KEY (HeadId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE Faculties (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Name NVARCHAR(100) NOT NULL UNIQUE,
+    DeanId INT NOT NULL,
+    FOREIGN KEY (DeanId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE Groups (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Name NVARCHAR(10) NOT NULL UNIQUE,
+    Year INT NOT NULL CHECK (Year BETWEEN 1 AND 5),
+    DepartmentId INT NOT NULL,
+    FOREIGN KEY (DepartmentId) REFERENCES Departments(Id)
+);
+
+CREATE TABLE GroupsCurators (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    CuratorId INT NOT NULL,
+    GroupId INT NOT NULL,
+    FOREIGN KEY (CuratorId) REFERENCES Teachers(Id),
+    FOREIGN KEY (GroupId) REFERENCES Groups(Id)
+);
+
+CREATE TABLE GroupsLectures (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    GroupId INT NOT NULL,
+    LectureId INT NOT NULL,
+    FOREIGN KEY (GroupId) REFERENCES Groups(Id),
+    FOREIGN KEY (LectureId) REFERENCES Lectures(Id)
+);
+
+CREATE TABLE Heads (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    TeacherId INT NOT NULL,
+    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE LectureRooms (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Building INT NOT NULL CHECK (Building BETWEEN 1 AND 5),
+    Name NVARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE Lectures (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    SubjectId INT NOT NULL,
+    TeacherId INT NOT NULL,
+    FOREIGN KEY (SubjectId) REFERENCES Subjects(Id),
+    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+);
+
+CREATE TABLE Schedules (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Class INT NOT NULL CHECK (Class BETWEEN 1 AND 8),
+    DayOfWeek INT NOT NULL CHECK (DayOfWeek BETWEEN 1 AND 7),
+    Week INT NOT NULL CHECK (Week BETWEEN 1 AND 52),
+    LectureId INT NOT NULL,
+    LectureRoomId INT NOT NULL,
+    FOREIGN KEY (LectureId) REFERENCES Lectures(Id),
+    FOREIGN KEY (LectureRoomId) REFERENCES LectureRooms(Id)
+);
+
+CREATE TABLE Subjects (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Teachers (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    Name NVARCHAR(max) NOT NULL,
+    Surname NVARCHAR(max) NOT NULL
+);
