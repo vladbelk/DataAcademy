f031a9764d48fa1423ad929f7a953da4634aadfb
vladbelk <vladbelk321@gmail.com>
2024-03-23 12:36:24 +0200

    Meeting_2

---
+++ script_2.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Groups (
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(10) NOT NULL UNIQUE,
+    Rating INT NOT NULL CHECK (Rating >= 0 AND Rating <= 5),
+    Year INT NOT NULL CHECK (Year >= 1 AND Year <= 5)
+);
+CREATE TABLE Departments (
+    Id INT PRIMARY KEY IDENTITY,
+    Financing MONEY NOT NULL DEFAULT 0,
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE Faculties (
+    Id INT PRIMARY KEY IDENTITY,
+    Name NVARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE Teachers (
+    Id INT PRIMARY KEY IDENTITY,
+    EmploymentDate DATE NOT NULL CHECK (EmploymentDate >= '1990-01-01'),
+    Name NVARCHAR(MAX) NOT NULL,
+    Premium MONEY NOT NULL DEFAULT 0 CHECK (Premium >= 0),
+    Salary MONEY NOT NULL CHECK (Salary >  0),
+    Surname NVARCHAR(MAX) NOT NULL
+);
